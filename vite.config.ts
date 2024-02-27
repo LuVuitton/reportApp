3ebbeea58f036e8e4697b6f551852aba51d366a0
FelixT <felixtekhada@gmail.com>
2024-02-27 21:55:54 +0100

    deploy

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import react from "@vitejs/plugin-react";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "https://luvuitton.github.io/reportApp",
+  base: "/reportApp/",
   build: {
     chunkSizeWarningLimit: 1600,
   },
